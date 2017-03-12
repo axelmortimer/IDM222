5cc5b1dfb8c60022afa1bee2ff72d62cfc333b0c
axelmortimer <axelmortimer@gmail.com>
2017-03-12 17:59:59 -0400

    Changed directory structure and links within files, fixed more html error

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     $("#hamburger").click(function() {
         $("nav ul").toggleClass("display");
-        if ($("#menu-icon").attr("src") === "../menu.svg") {
-            $("#menu-icon").attr("src", "../exit.png");
+        if ($("#menu-icon").attr("src") === "media/menu.svg") {
+            $("#menu-icon").attr("src", "media/exit.png");
         }
         else {
-            $("#menu-icon").attr("src", "../menu.svg");
+            $("#menu-icon").attr("src", "media/menu.svg");
         }
     })
 })
